8b79f1dc71b8a55728c5b5d884946bc5dfba96c8
Joao Carlos Santos <1162072@isep.ipp.pt>
2025-06-13 23:29:58 +0100

    registo html e php actualizados teste NOTOK

--- scripts/novoregisto.php
+++ scripts/novoregisto.php
@@ -1,28 +1,29 @@
 <?php
-
+echo 'teste';
 session_start();
 
 $db = new SQLite3('../base_dados.db');
 
 
-$name = $_POST["firstname"];
-$nif = $_POST["nif"]
-$username = $_POST['User'];
-$password = $_POST['Password'];
-$email = $_POST["email"];
-$contacto = $_POST["contacto"];
-$morada = $_POST["morada"];
+$Name = $_POST['Nome'];
+$Nif = $_POST['Nif'];
+$Username = $_POST['User'];
+$Password = $_POST['Password'];
+$Email = $_POST['Email'];
+$Contacto = $_POST['Contacto'];
+$Morada = $_POST['Morada'];
 
 
-$db->exec("INSERT INTO LOGIN (name, nif, username, password, email, contacto, morada) VALUES ('$name', '$nif', '$username', '$password', '$email', '$contacto', '$morada');");
+$db->exec("INSERT INTO LOGIN (Nome, Nif, User, Password, Email, Contacto, Morada) VALUES ('$Name', '$Nif', '$Username', '$Password', '$Email', '$Contacto', '$Morada');");
 
 $sqlvar = "select * from LOGIN ;";
 $result = $db->query($sqlvar);
 
-echo "$username";
-echo "$password";
+echo "$Username";
+echo "$Password";
 
 echo "<table>\n<th> id </th>\n<th> username </th>\n<th> password </th><th> email </th><th> contacto </th><th> morada </th>\n";
+echo '<tr><td>' . $row['id'] . '</td><td>' . $row['username'] . '</td><td>' . $row['password'] . '</td><td>' . $row['email'] . '</td><td>' . $row['contacto'] . '</td><td>' . $row['morada'] . "</td></tr>\n";
 
 
 ?>
