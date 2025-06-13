b29e3788dd71444223d13df9bfe0ccfa6eb1ac2f
Daniel Rodrigues <1240528@isep.ipp.pt>
2025-06-13 21:32:45 +0100

    Alterados os ficheiros para sem .html

--- scripts/login.php
+++ scripts/login.php
@@ -13,17 +13,17 @@ $result = $db->query($sqlvar);
 echo "$myUsername";
 echo "$myPassword";
 
-echo "<table>\n<th> id </th>\n<th> username </th>\n<th> password </th><th> email </th><th> contacto </th><th> morada </th>\n";
+//echo "<table>\n<th> id </th>\n<th> username </th>\n<th> password </th><th> email </th><th> contacto </th><th> morada </th>\n";
 
 while ($row = $result->fetchArray(SQLITE3_ASSOC))
 {
 if ($myUsername == $row['username'] && $myPassword == $row['password'])
 {
-    echo "funciona";
+   // Alert("Login bem sucedido.");
 
     }
 else{ echo "ERROR";}
-echo '<tr><td>' . $row['id'] . '</td><td>' . $row['username'] . '</td><td>' . $row['password'] . '</td><td>' . $row['email'] . '</td><td>' . $row['contacto'] . '</td><td>' . $row['morada'] . "</td></tr>\n";
+//echo '<tr><td>' . $row['id'] . '</td><td>' . $row['username'] . '</td><td>' . $row['password'] . '</td><td>' . $row['email'] . '</td><td>' . $row['contacto'] . '</td><td>' . $row['morada'] . "</td></tr>\n";
 
 }
 echo '</table>';
